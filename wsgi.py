8282fd5a0dab34500907badefd954f97276d51c4
flortheuns <flor.theuns@hotmail.com>
2019-07-25 12:55:09 +0200

    Try html

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,14 @@
-from flask import Flask
+import requests
+import json
+
+from flask import Flask, render_template
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hi World!"
+        
+    return
+render_template("home.html") 
 
 if __name__ == "__main__":
     application.run()
