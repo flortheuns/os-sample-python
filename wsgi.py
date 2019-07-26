a14e585be193db763de55b07f83e5c428d6ab2fc
flortheuns <flor.theuns@hotmail.com>
2019-07-26 14:40:42 +0200

    proper formatting

--- wsgi.py
+++ wsgi.py
@@ -11,17 +11,31 @@ def hello():
     token ='5AElSCMsMzdpIFbA1l1uW1yC'
     headers={'X-Vault-Token': token}
     r=requests.get(url, headers=headers, verify=False)
-    cert = r
-    key = " "
-    # r_json=r.json()
-    # data=r_json["data"]
-    # cert=data["cert"]
-    # key=data["key"]
-    # cert=cert.dumps()
-    # key=key.dumps()
+    r_json=r.json()
+    data=r_json["data"]
+    cert=data["cert"]
+    key=data["key"]
+    cert2 = '-----BEGIN CERTIFICATE-----'
+    key2 = '-----BEGIN RSA PRIVATE KEY-----'
+    check = 0
+    check2 = 0
+    for line in cert.split():
+        if line == "CERTIFICATE-----":
+            check = 1
+            continue
+        if check == 1:
+            cert2 += '\n'
+            cert2 += line
+    for line in key.split():
+        if line == "KEY-----":
+            check2 = 1
+            continue
+        if check2 == 1:
+            key2 += '\n'
+            key2 += line
     rendered = render_template('home.html', \
-        cert = cert, \
-        key = key)
+        cert = cert2, \
+        key = key2)
     return rendered
 
 if __name__ == "__main__":
